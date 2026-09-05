13bc04a74261686ec31aa6fb0e207757d469fe3e
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2026-09-05 06:44:30 +0300

    Add base url

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import indexText from "./index.txt";
 import helpText from "./help.txt";
 import { composeHTML, composeText } from "./text";
 
-async function renderPage(text: string, renderer: QRRenderer, isCode=true) {
+async function renderPage(text: string, renderer: QRRenderer, baseURL: string, isCode = true) {
   const tmpHtml = await Bun.file("./src/tmp.html").text();
   if (renderer === "html")
     return new Response(
       tmpHtml
         .replace("[CODE]", isCode ? "code" : "text")
-        .replace("[PAGE]", composeHTML(text, "http://localhost:3000")),
+        .replace("[PAGE]", composeHTML(text, baseURL)),
       { headers: { "content-type": "text/html" } },
     );
-  return new Response(composeText(text, "http://localhost:3000"));
+  return new Response(composeText(text, baseURL));
 }
 
 Bun.serve({
@@ -21,17 +21,18 @@ Bun.serve({
   routes: {
     "/*": (req) => {
       const url = new URL(req.url);
+      const baseURL = url.origin;
 
       const renderer = decideRenderer(url, (req.headers.get("user-agent") || "").toLowerCase());
-      if (url.pathname === "/help") return renderPage(helpText, renderer, false);
-      if (!url.search || url.search === "?") return renderPage(indexText, renderer, false);
+      if (url.pathname === "/help") return renderPage(helpText, renderer, baseURL, false);
+      if (!url.search || url.search === "?") return renderPage(indexText, renderer, baseURL, false);
 
       const data = String(url.searchParams.get("q") || url.search.slice(1));
       const options = getQrOptions(url);
       const type = getQrType(url, renderer);
       const qr = getQr(type, data, options);
 
-      return renderPage(qr, renderer);
+      return renderPage(qr, renderer, baseURL);
     },
   },
 });
